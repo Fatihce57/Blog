4298d3c84e8326f6f52a65de5893e7af816ab963
Fatih <fatihce57@gmail.com>
2021-11-30 23:04:38 +0100

    project done

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,29 +1,30 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import Header from '../../components/header/Header';
-import Post from '../../components/post/Post';
-import Sidebar from '../../components/sidebar/Sidebar';
-import './home.css;';
-
+import { useEffect, useState } from "react";
+import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./home.css";
+import axios from "axios";
+import { useLocation } from "react-router";
 
 export default function Home() {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            const res = await axios.get("/posts")
-            console.log(res)
-        }
-        fetchPosts()
-    }, [])
-
-    return (
-        <>
-            <Header />
-            <div className="home">
-                <Post />
-                <Sidebar />
-            </div>
-        </>
-    )
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts" + search);
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, [search]);
+  return (
+    <>
+      <Header />
+      <div className="home">
+        <Posts posts={posts} />
+        <Sidebar />
+      </div>
+    </>
+  );
 }
+
